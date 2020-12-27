8998760b9466f35e15f664c6eb17221103b9acff
rurikjart <rurik.j.art@gmail.com>
2020-12-27 17:18:03 +0300

    Task 3. Готовим сервер к интеграции с БД postgresql rmes (Reminder Me Server)

---
+++ src/main/java/org/ut/rmes/config/DatabaseConfig.java
@@ -0,0 +1,21 @@
+package org.ut.rmes.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.Resource;
+
+@Configuration
+@EnableJpaRepositories("org.ut.rmes.repository")
+@EnableTransactionManagement
+@ComponentScan("org.ut.rmes")
+@PropertySource("classpath:db.properties")
+public class DatabaseConfig {
+
+    @Resource
+    private Environment env;
+}
